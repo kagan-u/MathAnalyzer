121b624c94f66081fdc5888872d24e9d68ddcb16
kagan-u <kaganunal.15@gmail.com>
2025-04-04 22:51:39 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .basic_math import factorial, is_prime, fibonacci
+from .stats import mean, median, mode, variance
+from .data_processor import clean_list, sort_list, filter_list
+
+__all__ = [
+    'factorial', 'is_prime', 'fibonacci',
+    'mean', 'median', 'mode', 'variance',
+    'clean_list', 'sort_list', 'filter_list'
+]
